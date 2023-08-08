6daf873fb052761a01a2cee526057b17cb16d62a
Minister <mundestephane13@gmail.com>
2023-08-08 02:40:57 +0100

    @angular/core migration - migration-v11-router-relative-link-resolution-default
    The default value for `relativeLinkResolution` is changing from 'legacy' to 'corrected'.
    This migration updates `RouterModule` configurations that use the default value to
    now specifically use 'legacy' to prevent breakages when updating.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -37,7 +37,8 @@ const appRoutes: Routes = [
 ];
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes, {
-    initialNavigation: 'enabled'
+    initialNavigation: 'enabled',
+    relativeLinkResolution: 'legacy'
 })],
   exports: [RouterModule]
 })
